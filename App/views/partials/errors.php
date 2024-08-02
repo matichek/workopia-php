69017d3c4d3fadb648b7362c461adb9e753f040f
Matija Štrlekar <mstrlekar@gmail.com>
2024-08-02 17:37:14 +0200

    init

--- App/views/partials/errors.php
+++
@@ -1,5 +0,0 @@
-<?php if (isset($errors)) : ?>
-  <?php foreach ($errors as $error) : ?>
-    <div class="message bg-red-100 my-3"><?= $error ?></div>
-  <?php endforeach; ?>
-<?php endif; ?>
--- public/index.php
+++ public/index.php
@@ -1,21 +1,6 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-
-use Framework\Router;
-use Framework\Session;
-
-Session::start();
 
 require '../helpers.php';
 
-// Instatiate the router
-$router = new Router();
+require basePath('views/home.view.php');
 
-// Get routes
-$routes = require basePath('routes.php');
-
-// Get current URI and HTTP method
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Route the request
-$router->route($uri);
