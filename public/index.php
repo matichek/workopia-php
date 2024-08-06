7f38665acd16854eb8b681806f7d318a24f9308f
Matija Štrlekar <mstrlekar@gmail.com>
2024-08-06 20:20:05 +0200

    chore: Update route for creating listings

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require '../helpers.php';
 $routes = [
     '/' => 'controllers/home.php',
     '/listings' => 'controllers/listings/index.php',
-    'listings/create' => 'controllers/listings/create.php',
+    '/listings/create' => 'controllers/listings/create.php',
     '404' => 'controllers/error/404.php'
 ];
 
