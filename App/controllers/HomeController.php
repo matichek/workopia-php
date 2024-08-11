20ebbea6d2cbd49f69349c14b75dd756855a100b
Matija Štrlekar <mstrlekar@gmail.com>
2024-08-11 13:30:29 +0200

    Update home view to use HomeController@index

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -1,12 +1,27 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
+use Framework\Database;
+
+
 class HomeController
 {
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath("config/db.php");
+        $this->db = new Database($config);
+    }
 
     public function index()
     {
-        return loadView('home');
+        
+        $listings = $this->db->query("SELECT * FROM listings")->fetchAll();
+
+        loadView('home', [
+            'listings' => $listings
+        ]);
     }
 }
--- App/controllers/home.php
+++ App/controllers/home.php
@@ -1,12 +1,3 @@
 <?php 
 
 use Framework\Database;
-
-$config = require basePath("config/db.php");
-$db = new Database($config);
-
-$listings = $db->query("SELECT * FROM listings")->fetchAll();
-
-loadView('home', [
-    'listings' => $listings
-]);
