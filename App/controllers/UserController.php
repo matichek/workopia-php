786de4693b3bae1da38e74aeeec858149d84baf7
Matija Štrlekar <mstrlekar@gmail.com>
2024-09-01 19:12:13 +0200

    Login, register setup

---
+++ App/controllers/UserController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+
+class UserController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    /**
+     * Show the login view.
+     *
+     * @return void
+     */
+    public function login()
+    {
+        loadView('users/login');
+    }
+
+    /**
+     * Show the registration view.
+     *
+     * @return void
+     */
+    public function create()
+    {
+        loadView('users/create');
+    }
+
+    public function store()
+    {
+
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $password = $_POST['password'];
+        $password_confirmation = $_POST['password_confirmation'];
+        $state = $_POST['state'];
+
+        $errors = [];
+
+        // Validation
+
+        if (!Validation::email($email)) {
+            $errors['email'] = 'Please enter valid email.';
+        }
+
+        if (!Validation::string($name, 2, 50)) {
+            $errors['name'] = 'Name must be between 2 and 50 characters.';
+        }
+
+
+        if (!Validation::string($password, 6, 50)) {
+            $errors['password'] = 'Password must be min 6 characters';
+        }
+
+        if (!Validation::match($password, $password_confirmation)) {
+            $errors['password_conformation'] = 'Passwords do not match';
+        }
+
+
+        if (!empty($errors)) {
+            loadView('users/create', [
+                'errors' => $errors,
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,
+                    'city' => $city,
+                    'state' => $state,
+                ]
+            ]);
+            exit;
+        } 
+
+        // Check if email already exists
+
+        $params = [
+            'email' => $email
+        ];
+
+        $user = $this->db->query('SELECT * FROM users WHERE email = :email', $params)->fetch();
+
+        if ($user) {
+            $errors['email'] = 'Email already exists';
+
+            loadView('users/create', [
+                'errors' => $errors
+            ]);
+            exit;
+        }
+
+        // Insert user into database
+
+        $params = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ];
+
+        $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        redirect('/auth/login');
+
+        
+        
+
+        exit;
+
+    }
+}
